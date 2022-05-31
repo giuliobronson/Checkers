ff7447ba6cd28cc006f9a5ecb754cefead397797
queirozime <carloshenricco@gmail.com>
2022-05-31 20:31:28 -0300

    fix mem alocation

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,11 @@ class Tabuleiro {
         Casa ***casas;
     public:
         Tabuleiro() {
+            casas = new Casa**[8];
+            for(int i = 0; i < 8; i++) {
+                casas[i] = new Casa*[8];
+            }
+
             for (int i = 0; i < 8; i++) {
                 for(int j = 0; j < 8; j++) {
                     if(i <= 2 && i%2 != j%2) {
